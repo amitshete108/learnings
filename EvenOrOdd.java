176bc6feb72e4286151875b7ef6c7be3a0ce4959
amitshete108 <sheteamit108@gmail.com>
2024-03-02 14:28:41 +0530

    Fixed the defect #123 for EvenOrOdd

---
+++ EvenOrOdd.java
@@ -0,0 +1,23 @@
+package JAVA_CERTIFICATION;
+import java.util.*;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		
+		System.out.println("To find number even or odd");
+		System.out.println();
+		System.out.println("Please Enter the Number");
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=a%2;
+		
+		if(b==0) {
+			System.out.println("Entered Number is Even");
+			}
+		else {
+			System.out.println("Entered Number is Odd");
+		}
+	}
+
+}
