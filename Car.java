76fc5edddb8eacc60eac0dee0e518dc14718c815
amitshete108 <sheteamit108@gmail.com>
2024-03-02 14:49:05 +0530

    Added new files for exception handling

---
+++ Car.java
@@ -0,0 +1,50 @@
+package ClassAndObjects;
+
+import java.util.*;
+
+public class Car {
+	private String company;
+	private String carName;
+
+	static int noOfCars=0;
+	static List<String> names=new ArrayList();
+
+	public Car(String company, String carName) {
+		this.company=company;
+		this.carName=carName;
+		noOfCars++;
+	}
+
+	public void StringCompany(String company) {
+		this.company=company;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+	public void StringcarName(String carName) {
+		this.carName=carName;
+	}
+
+	public String getcarName() {
+		return carName;
+	}
+
+	//Static Methods
+
+	public static void setNoOfCars(int no) {
+		noOfCars=no;
+	}
+	public static int getNoOfCars() {
+		return noOfCars;
+	}
+
+	static {
+		names.add("Car1");
+		names.add("Car2");
+		names.add("Car3");
+		names.add("Car4");
+
+	}	
+
+}
---
+++ Pen.java
@@ -0,0 +1,17 @@
+package ClassAndObjects;
+
+public class Pen {
+
+	//Attributes
+	String color,brand,shape,material;
+	double price;
+
+	//Member functions or methods or behavior
+	public void write() {
+		System.out.println("Writing with Pen");
+	}
+
+	public void refill() {
+		System.out.println("Refilling the Pen");
+	}
+}
